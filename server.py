ef7e796b75797bfb0fa88962ee4e3c59cf103ce6
Rishab Hegde <rishabhegde@Rishabs-MacBook-Pro.local>
2017-03-12 19:27:10 -0700

    lol

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+import json
 
 app = Flask(__name__)
 
@@ -12,8 +13,8 @@ def hello():
   w1 = random.randrange(len(words))
   w2 = random.randrange(len(words))
   w3 = random.randrange(len(words))
-  s = words[w1] + " " + words[w2] + " " + words[w3]
-  return s
+  s = words[w1].strip() + " " + words[w2].strip() + " " + words[w3].strip()
+  return json.dumps({'words': s})
 
 
 if __name__ == "__main__":
